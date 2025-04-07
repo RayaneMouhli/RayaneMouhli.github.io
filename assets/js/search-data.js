42cbed0fa965785c30e4005809b397b760ce1d0c
RayaneMouhli <63062731+RayaneMouhli@users.noreply.github.com>
2025-04-07 00:12:49 +0000

    Deploying to gh-pages from @ RayaneMouhli/RayaneMouhli.github.io@fbc696d1a2958da790bfba81c2d51a5f12a94952 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-tv-i-gave-a-talk-at-the-congrès-des-jeunes-chercheurs-en-mathématiques-appliquées-cjc-ma-in-lyon-on-the-decorrelation-of-vector-fields-with-speed-of-varifolds",
+          title: ':tv: I gave a talk at the Congrès des Jeunes Chercheurs en Mathématiques...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
