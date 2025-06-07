0505cd6b2544740bb4c0fd942b3786cce41ae924
RayaneMouhli <63062731+RayaneMouhli@users.noreply.github.com>
2025-06-07 10:20:35 +0000

    Deploying to gh-pages from @ RayaneMouhli/RayaneMouhli.github.io@dfe2305a667b0790ba1768dd448ce302fdc16014 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,10 +42,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-gave-a-talk-at-the-congrès-des-jeunes-chercheurs-en-mathématiques-appliquées-cjc-ma-in-lyon-on-the-decorrelation-of-vector-fields-with-speed-of-varifolds-slides",
-          title: 'I gave a talk at the Congrès des Jeunes Chercheurs en Mathématiques Appliquées...',
-          description: "",
-          section: "News",},{id: "news-i-gave-a-talk-at-the-workshop-infinite-dimensional-geometry-theory-and-applications-at-erwin-schrodinger-institute-in-vienna-austria-on-the-decorrelation-of-vector-fields-with-speed-of-varifolds-slides-video",
+            },},{id: "news-i-gave-a-talk-at-the-workshop-infinite-dimensional-geometry-theory-and-applications-at-erwin-schrodinger-institute-in-vienna-austria-on-the-decorrelation-of-vector-fields-with-speed-of-varifolds-slides-video",
           title: 'I gave a talk at the workshop “Infinite-dimensional Geometry : Theory and Applications”...',
           description: "",
           section: "News",},{id: "news-i-gave-a-talk-at-the-map5-lab-phds-seminar-about-lddmm-and-decorrelation-of-vector-fields-using-speed-of-varifolds-slides",
