cedb6c87c34dab07e0823359fb547c80029938f2
RayaneMouhli <63062731+RayaneMouhli@users.noreply.github.com>
2025-11-19 12:51:00 +0000

    Deploying to gh-pages from @ RayaneMouhli/RayaneMouhli.github.io@ca670cb925b82fedb25b55a4b37a7333aa3f57e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "Publications",
+  },{id: "nav-research",
+          title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/research/";
           },
         },{id: "nav-talks",
           title: "Talks",
